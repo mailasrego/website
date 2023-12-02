744d1f6b0f394ce994716bd4a6745fc35a0c5ea1
mailasrego <132529115+mailasrego@users.noreply.github.com>
2023-12-02 13:37:44 +1100

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":977},"pages":[{"id":"page-design","displayName":"Design","link":{"linkType":"LinkTypePage","href":"#!page-design"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-photo","displayName":"Photo","link":{"linkType":"LinkTypePage","href":"#!page-photo"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-code","displayName":"Code","link":{"linkType":"LinkTypePage","href":"#!page-code"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+<script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208413647">

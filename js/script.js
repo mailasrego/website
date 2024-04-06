e8eaabb44ef5a964f3c0ae3e4abef660d66c11ee
mailasrego <132529115+mailasrego@users.noreply.github.com>
2024-04-06 13:57:54 +1100

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":977},"pages":[{"id":"page-design","displayName":"Design","link":{"linkType":"LinkTypePage","href":"#!page-design"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-photo","displayName":"Photo","link":{"linkType":"LinkTypePage","href":"#!page-photo"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-code","displayName":"Code","link":{"linkType":"LinkTypePage","href":"#!page-code"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
-<script src="//widget.simplybook.net/v2/widget/widget.js" type="text/javascript">
-
-    var widget = new SimplybookWidget({"widget_type":"button","url":"https:\/\/googscape.simplybook.net","theme":"emeri","theme_settings":{"timeline_hide_unavailable":"1","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","sb_base_color":"#7b8a78","display_item_mode":"block","booking_nav_bg_color":"#ffffff","body_bg_color":"#f6f6f8","sb_review_image":"4","sb_review_image_preview":"\/uploads\/googscape\/image_files\/preview\/eddbff7f4bf8757d9e18ac7cee460abb.png","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#b0c3ac","sb_company_label_color":"#485146","hide_img_mode":"0","sb_busy":"#c7b3b3","sb_available":"#e2eaec"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]},"button_title":"Request an onsite visit","button_background_color":"#7b8a78","button_text_color":"#ffffff","button_position":"right","button_position_offset":"55%"});
+<script type="text/javascript">
+    (function(w, d, s, i) {
+        var script = d.createElement(s);
+        script.async = true;
+        script.src = "//widget.simplybook.net/v2/widget/widget.js";
+        script.onload = function() {
+            new SimplybookWidget({"widget_type":"button","url":"https:\/\/googscape.simplybook.net","theme":"emeri","theme_settings":{"timeline_hide_unavailable":"1","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","sb_base_color":"#7b8a78","display_item_mode":"block","booking_nav_bg_color":"#ffffff","body_bg_color":"#f6f6f8","sb_review_image":"4","sb_review_image_preview":"\/uploads\/googscape\/image_files\/preview\/eddbff7f4bf8757d9e18ac7cee460abb.png","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#b0c3ac","sb_company_label_color":"#485146","hide_img_mode":"0","sb_busy":"#c7b3b3","sb_available":"#e2eaec"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":{"provider":"2","service":"2"}},"button_title":"Request an onsite visit","button_background_color":"#7b8a78","button_text_color":"#ffffff","button_position":"right","button_position_offset":"55%","container_id":"sbw_i4j42o"});
+        };
+        d.head.appendChild(script);
+    })(window, document, 'script', 'sbw_i4j42o');
